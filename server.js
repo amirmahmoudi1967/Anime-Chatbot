f4e559c505b405aceb8bb56b7aa2e92e663f1749
Amir_mmdi <47902830+amirmahmoudi1967@users.noreply.github.com>
2021-03-27 14:10:18 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+`use strict`;
+
+const matcher = require('./matcher');
+const {getEpisode} = require('./jikan');
+const express = require (`express`) ;
+const bodyparser = require (`body-parser`) ;
+const config = require(`./config`);
+const FBeamer = require('./FBeamer');
+const jikan = require('./jikan');
+const f=new FBeamer(config.FB);
+const server = express () ;
+const PORT = process.env.PORT||3000;
+const sendResponse=require('./anime_bot');
+
+server.post (`/`, bodyparser.json ({
+    verify : f.verifySignature.call (f)
+    }) ) ;
+    server.post('/',(req,res,next)=>{
+        return f.incoming(req,res,async data=>{
+            try{
+                if(data && data.type==='text'){
+                    await sendResponse(f,data.sender,data.content);
+                }
+            }catch(e){
+                console.log(e);
+            }
+        });
+    });
+        
+
+server.get(`/`,(req,res) => f.registerHook(req,res));
+server.listen (PORT , () => console.log ( `The bot server is
+running on port ${ PORT } `) ) ;
